f99d688c0346049906d29443ab69c3ec431f072b
enomorino <enesherifi5@gmail.com>
2024-02-19 19:01:31 +0100

    Step 1 of plan complete
    Code for step one, which included drawing the background, with blinking stars, planet floor, and UFO.

---
+++ .github/lunar-lander.js
@@ -0,0 +1,142 @@
+
+let width = 600;
+let height = 600;
+let starsGenerated = false;
+let starX = [];
+let starY = [];
+let starAlpha = [];
+let centerX = 300;
+
+function setup() {
+    createCanvas(width, height);
+
+}
+
+
+//Sky
+function spaceSky(){
+    background(48, 25, 52);
+    noStroke();
+    fill(255,245,255);
+    rect(0,450,width);
+
+    fill(245, 245, 245);
+    ellipse(95, 520, 60,40); 
+    ellipse(220,500, 70, 30);
+    ellipse(420, 510, 60, 30);
+
+    fill(211,211,211);
+    ellipse(95, 520, 50, 30);
+    ellipse(220, 500, 60, 20);
+    ellipse(420, 510, 50, 20);
+    ellipse(20, 460, 30, 10);
+    ellipse(500, 465, 50, 20);
+    ellipse(105, 475, 30, 10);
+    ellipse(530, 510, 60, 30);
+
+    fill(233, 200, 245);
+    ellipse(width-150, height - 500, 90);
+    fill(233, 200, 245, 80);
+    ellipse(width-150, height - 500, 120);
+    fill(233, 200, 245, 60);
+    ellipse(width-150, height - 500, 150);
+
+    if(!starsGenerated) {
+        push();
+        for (let i = 0; i < 400; i ++){
+            const x = Math.floor(Math.random () * width);
+            const y = Math.floor(Math.random () * height);
+            const alpha = Math.random();
+            starX.push(x);
+            starY.push(y);
+            starAlpha.push(alpha);
+        }
+        starsGenerated = true;
+        pop();
+
+    }
+
+}
+
+//Stars
+
+function ufo(){
+    stroke(87, 87, 87);
+    strokeWeight(40);
+    strokeJoin(ROUND);
+    fill(87,87,87);
+    rect(285, 355, 30, 3);
+    stroke(97, 40, 64);
+    fill(97, 40, 64);
+    triangle(300, 200, 250, 255, 350, 255);
+    rect(270,275, 60,60);
+    noStroke();
+    fill(87, 87, 87);
+    ellipse(300, 240, 45);
+    fill(211,211,211);
+    ellipse(300, 240, 35);
+    fill(87, 30, 56);
+    ellipse(260, 275, 4);
+    ellipse(270, 275, 4);
+    ellipse(280, 275, 4);
+    ellipse(290, 275, 4);
+    ellipse(300, 275, 4);
+    ellipse(310, 275, 4);
+    ellipse(320, 275, 4);
+    ellipse(330, 275, 4);
+    ellipse(340, 275, 4);
+    ellipse(260, 285, 4);
+    ellipse(260, 295, 4);
+    ellipse(260, 305, 4);
+    ellipse(260, 315, 4);
+    ellipse(260, 325, 4);
+    ellipse(260, 335, 4);
+    ellipse(260, 345, 4);
+    ellipse(340, 285, 4);
+    ellipse(340, 295, 4);
+    ellipse(340, 305, 4);
+    ellipse(340, 315, 4);
+    ellipse(340, 325, 4);
+    ellipse(340, 335, 4);
+    ellipse(340, 345, 4);
+    ellipse(300, 195, 4);
+    ellipse(240, 260, 4);
+    ellipse(300, 195, 4);
+    ellipse(360, 260, 4);
+    stroke(87, 87, 87);
+    strokeJoin(BEVEL);
+    beginShape();
+    vertex(300, 320);
+    vertex(300, 355);
+    vertex(300, 320);
+    endShape();
+    strokeWeight(30);
+    stroke(211,211,211);
+    beginShape();
+    vertex(300, 320);
+    vertex(300, 355);
+    vertex(300, 320);
+    endShape();
+    strokeWeight(2);
+    stroke(87, 87, 87);
+    line(290, 335, 293, 335);
+}
+
+function draw(){
+ 
+ spaceSky();
+ 
+ noStroke();
+ for (let index in starX){
+    fill(255,255,255, Math.abs(Math.sin(starAlpha [index ])* 255));
+    ellipse(starX [index], starY [index], 1);
+    starAlpha [index] = starAlpha [index] + 0.02;
+ }
+ ufo();
+
+}
+
+
+
+
+
